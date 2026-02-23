72e19e17f5af07accf374e4ca9d305f6766fa2eb
kallezz <kalle.melender@hotmail.com>
2026-02-23 13:52:55 +0200

    add database connection to main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
+from sqlalchemy.orm import sessionmaker
+
+from src.database.db import engine
+from src.database.models.base import Base
+
+
+# Create all tables in the database
+Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Everything else here:
 print("Hello world")
+
+# Close session
+session.close()
