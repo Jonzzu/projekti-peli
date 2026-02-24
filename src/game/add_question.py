df163baa9e97820d07aa5d9b3d5c8ece8a87b49b
kallezz <kalle.melender@hotmail.com>
2026-02-24 13:18:45 +0200

    change requirements for add_question.py

--- src/game/add_question.py
+++ src/game/add_question.py
@@ -1,6 +1,6 @@
 def run_add_question():
     # TODO: Koodaa funktio jolla käyttäjä voi lisätä databaseen uuden kysymyksen
-    # Vaatimukset: Puhdista terminaali, pyydä käyttäjää syöttämään kysymys,
+    # Vaatimukset: Pyydä käyttäjää syöttämään kysymys,
     # pyydä käyttäjää syöttämään väh. kolme vastausta, jos syötetään tyhjä niin tallenna kysymys vastauksineen
     # jos tyhjä syötetään ennen kuin 3 vastausta on annettu, pyydä lisää vastauksia
     # ennen tallennusta tulosta kysymys ja numeroitu lista annetuista vastauksista,
